2d3d7c3505b98fcfb032fb0b46b6146c35ac9b5f
BKimberlin99 <bkimberl@highpoint.edu>
2019-05-03 12:12:53 -0400

    Put in missing file.

---
+++ Photorama/Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Brandon Kimberlin on 5/3/19.
+//  Copyright © 2019 Brandon Kimberlin. All rights reserved.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImage(for: photo) { (result) -> Void in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+}
