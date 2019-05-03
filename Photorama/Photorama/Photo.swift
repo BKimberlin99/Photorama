05249ce65bdf8c3de7db510a8d5236dde1063756
BKimberlin99 <bkimberl@highpoint.edu>
2019-05-03 11:24:54 -0400

    Finished chapter 20

---
+++ Photorama/Photorama/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Brandon Kimberlin on 5/3/19.
+//  Copyright © 2019 Brandon Kimberlin. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.photoID = photoID
+        self.remoteURL = remoteURL
+        self.dateTaken = dateTaken
+    }
+}
